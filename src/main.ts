9e38b8fec8e992a79d1082d5b6fab8f60b75ba1c
Okedele <okedelep@gmail.com>
2021-07-25 11:52:29 +0100

    created project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  const PORT = process.env.PORT || 3000;
+  const nodeEnv = process.env.NODE_ENV || 'development';
+
+  await app.listen(PORT, () => {
+    console.log(`Application running in ${nodeEnv} mode on port ${PORT}`);
+  });
+}
+bootstrap();
